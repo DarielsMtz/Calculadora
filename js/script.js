c2a2844d48a69db1206a08932c1f004a5202b61b
DarielsMtz <127892871+DarielsMtz@users.noreply.github.com>
2024-02-27 15:09:08 +0100

    Actualización de estilos y nombres de clases en CSS y HTML, y refactorización del script JavaScript

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let resultado = document.getElementById("resultado");
-  let btns = document.querySelectorAll(".tecla");
-  let evento_final = "";
+  let resultado = document.getElementById("resultado"); // Seleccionar el elemento con el id resultado
+  let btns = document.querySelectorAll(".btn_calculadora"); // Seleccionar todos los botones de la calculadora
+  let evento_final = ""; // Variable para controlar el último evento
 
   // Lista de errores que se pueden producir
   const lista_errores = [
@@ -17,25 +17,31 @@ document.addEventListener("DOMContentLoaded", function () {
   // Función para realizar la operación matemática
   function calcular_operacion(calculo) {
     try {
+      // Evaluar la operación matemática
       return eval(calculo);
     } catch (error) {
+      // En caso de que se produzca un error, devolver el nombre del error
       return error.name;
     }
   }
 
-  // Evento click para cada botón
+  // Evento click para cada botón de los números y operadores
   function btn_igual() {
-    resultado.textContent = calcular_operacion(resultado.textContent);
+    if (!lista_errores.includes(resultado.textContent)) {
+      resultado.textContent = calcular_operacion(resultado.textContent);
+    }
+    // Cambiar el valor de la variable evento_final
     evento_final = "Igual";
   }
 
   // Función para manejar el botón AC
   function btn_AC() {
     resultado.textContent = "0";
+    evento_final = "";
   }
 
   // Función para manejar los botones de número y operador
-  function manejarBotonNumeroOperador(btn_clickeado) {
+  function manejar_input(btn_clickeado) {
     if (lista_errores.includes(resultado.textContent)) {
       resultado.textContent = btn_clickeado;
     } else {
@@ -51,6 +57,7 @@ document.addEventListener("DOMContentLoaded", function () {
   //  Función para agregar el evento click a cada botón
   function agregar_evento_click(btn_evento) {
     btn_evento.addEventListener("click", function () {
+      // Obtener el texto del botón clickeado
       const btn_presionado = btn_evento.textContent;
 
       // Sentencias switch para manejar los botones
@@ -65,7 +72,7 @@ document.addEventListener("DOMContentLoaded", function () {
           break;
         // En el caso de que el botón clickeado sea un número o un operador
         default:
-          manejarBotonNumeroOperador(btn_presionado);
+          manejar_input(btn_presionado);
           break;
       }
     });
