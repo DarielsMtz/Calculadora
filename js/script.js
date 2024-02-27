ae5f426d1531ea5c14659b1cb175f66e96c887b8
DarielsMtz <127892871+DarielsMtz@users.noreply.github.com>
2024-02-27 18:23:31 +0100

    Actualizar script.js con cambios en la calculadora

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let resultado = document.getElementById("resultado"); // Seleccionar el elemento con el id resultado
-  let btns = document.querySelectorAll(".btn_calculadora"); // Seleccionar todos los botones de la calculadora
-  let evento_final = ""; // Variable para controlar el último evento
+  // Seleccionar el elemento con el id resultado
+  let resultado = document.getElementById("resultado");
+
+  // Seleccionar todos los botones de la calculadora
+  let btns = document.querySelectorAll(".btn_calculadora");
+
+  // Variable para controlar el último evento
+  let ultima_accion = "";
 
   // Lista de errores que se pueden producir
   const lista_errores = [
+    "0",
     "Error",
     "Infinity",
     "-Infinity",
@@ -13,6 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
     "SintaxError",
   ];
 
+  // ------------------------------------------------------
   // Función para realizar la operación matemática
   function calcular_operacion(calculo) {
     try {
@@ -24,36 +31,39 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
+  // ------------------------------------------------------
   // Evento click para cada botón de los números y operadores
   function btn_igual() {
     if (!lista_errores.includes(resultado.textContent)) {
       resultado.textContent = calcular_operacion(resultado.textContent);
     }
-    // Cambiar el valor de la variable evento_final
-    evento_final = "Igual";
+    // Cambiar el valor de la variable que controla el último evento
+    ultima_accion = "Igual";
   }
 
+  // ------------------------------------------------------
   // Función para manejar el botón AC
   function btn_AC() {
     resultado.textContent = "0";
-    evento_final = "";
+    ultima_accion = "";
   }
 
+  // ------------------------------------------------------
   // Función para manejar los botones de número y operador
   function manejar_input(btn_clickeado) {
     if (lista_errores.includes(resultado.textContent)) {
       resultado.textContent = btn_clickeado;
     } else {
-      // En caso de que el último evento sea el botón igual
-      if (evento_final === "Igual") {
+      if (ultima_accion === "Igual") {
         resultado.textContent = btn_clickeado;
-        evento_final = "";
+        ultima_accion = "";
       } else {
         resultado.textContent += btn_clickeado;
       }
     }
   }
 
+  // ------------------------------------------------------
   //  Función para agregar el evento click a cada botón
   function agregar_evento_click(btn_evento) {
     btn_evento.addEventListener("click", function () {
